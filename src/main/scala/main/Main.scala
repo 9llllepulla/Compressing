6b9176a996136fcdf371bff3a4639ef74dfd48d0
sergeylyashko <slyashko@mail.ru>
2022-01-08 15:28:15 +0300

    added uncomress text display, count compression

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -5,8 +5,18 @@ object Main extends App {
   val compress = Compressor
 
   val text = "SUSIE SAYS IT IS EASY"
+
+  val compresslessText = text.toList
+    .map(ch => Integer.toBinaryString(ch))
+    .foldLeft("")(_ + _)
+  println("uncompressed code: " + compresslessText)
+  println("full length: " + compresslessText.length)
+
   val compressingText = compress.code(text)
-  println(compressingText)
+  println("compressed code: " + compressingText)
+  println("compressed length: " + compressingText.length)
+
+  println("compressed in: " + (compresslessText.length / (1f * compressingText.length)))
 
   val decompressingText = compress.decode(compressingText)
   println(decompressingText)
